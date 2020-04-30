b9e77e9fdcf8193a09dd9de556dde07363b1a49c
Edmund Hee <edmund.hee05@gmail.com>
2020-04-30 22:50:38 +0800

    Fix responsive issue

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
     <contentWrapper id="home" :bg_img="'../img/bg_1.jpg'">
       <template v-slot:content>
         <div class="lg:pl-20 pb-20">
-            <div class="font-sans font-light text-8xl text-white transition duration-500 ease-in-out lg:text-gray-800">Begin</div>
-            <div class="font-sans font-bold text-8xl text-white transition duration-500 ease-in-out lg:text-gray-800 sub-topic">Anywhere</div>
+            <div class="font-sans font-light text-5xl mb-2 text-white transition duration-500 ease-in-out md:mb-0 md:text-8xl lg:text-gray-800">Begin</div>
+            <div class="font-sans font-bold text-5xl text-white mb-2 transition duration-500 ease-in-out md:mb-0 md:text-8xl lg:text-gray-800 sub-topic">Anywhere</div>
             <div class="w-20 bg-white transition duration-500 ease-in-out lg:bg-gray-800 separator-line"></div>
-            <div class="font-sans font-thin text-3xl text-white transition duration-500 ease-in-out lg:text-gray-800 description">Your passion will guide you</div>
+            <div class="font-sans font-thin text-2xl text-white transition duration-500 ease-in-out lg:text-gray-800 description">Your passion will guide you</div>
         </div>
       </template>
     </contentWrapper>
